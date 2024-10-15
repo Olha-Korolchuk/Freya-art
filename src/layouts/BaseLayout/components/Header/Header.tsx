15d0f2d637fe7deecd8bfd115f595f06c113e1cf
Olha-Korolchuk <korolchuk.olha@student.uzhnu.edu.ua>
2024-10-15 22:24:58 +0300

    FA-2: Created Sign up page

--- src/layouts/BaseLayout/components/Header/Header.tsx
+++ src/layouts/BaseLayout/components/Header/Header.tsx
@@ -1,20 +1,20 @@
-import { LINK_TEMPLATES } from "@/constants/link";
-import { StyledHeader, StyledImg, StyledLink, StyledNavs } from "./styles";
-import ImageLogo from "@/assets/images/logo.png";
-import React from "react";
+import { LINK_TEMPLATES } from '@/constants/link';
+import { StyledHeader, StyledImg, StyledLink, StyledNavs } from './styles';
+import ImageLogo from '@/assets/images/logo.png';
+import React from 'react';
 
 export const Header = () => {
-  return (
-    <StyledHeader>
-      <StyledImg src={ImageLogo} />
-      <StyledNavs>
-        <StyledLink to={LINK_TEMPLATES.HOME} isContained={true}>
-          Sign in
-        </StyledLink>
-        <StyledLink to={LINK_TEMPLATES.HOME} isContained={false}>
-          Sign up
-        </StyledLink>
-      </StyledNavs>
-    </StyledHeader>
-  );
+    return (
+        <StyledHeader>
+            <StyledImg src={ImageLogo} />
+            <StyledNavs>
+                <StyledLink to={LINK_TEMPLATES.HOME} isContained={true}>
+                    Sign in
+                </StyledLink>
+                <StyledLink to={LINK_TEMPLATES.SIGN_UP} isContained={false}>
+                    Sign up
+                </StyledLink>
+            </StyledNavs>
+        </StyledHeader>
+    );
 };
